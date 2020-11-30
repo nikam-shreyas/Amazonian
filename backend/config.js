2086790556f69cae36eea69a4a9480859ad3be77
ZeronP <nik4m.5hreyas@gmail.com>
2020-11-30 17:46:16 +0530

    Deploying

--- backend/config.js
+++ backend/config.js
@@ -8,5 +8,5 @@ export default {
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || "sb",
   accessKeyId: process.env.accessKeyId || "accessKeyId",
-  secretAccessKey: process.env.secretAccessKey || "secretAccessKey",
+  secretAccessKey: process.env.secretAccessKey || "secretAccessKey"
 };
